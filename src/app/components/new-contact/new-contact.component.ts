c274e7464bc233806b818a088cb96f2ba7f4f416
Ryhor Sidoryn <rigor1988@gmail.com>
2020-11-14 23:42:21 +0300

    feat: get data from popup window

--- src/app/components/new-contact/new-contact.component.ts
+++ src/app/components/new-contact/new-contact.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserData } from '../../common/utils/user';
 
 @Component({
   selector: 'app-new-contact',
   templateUrl: './new-contact.component.html',
-  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./new-contact.component.css']
 })
 export class NewContactComponent implements OnInit {
@@ -26,9 +25,9 @@ export class NewContactComponent implements OnInit {
 
   createNewQuestion() {
 		this.newUserForm = new FormGroup({
-			name: new FormControl("", [Validators.pattern(/[A-z]/)]),
-      phone: new FormControl("", [Validators.pattern(/[0-9]/)]),
-      email: new FormControl("", [Validators.email]),
+			"name": new FormControl("", [Validators.pattern(/[A-z]/)]),
+      "phone": new FormControl("", [Validators.pattern(/[0-9]/)]),
+      "email": new FormControl("", [Validators.email]),
     });
 	}
 
@@ -42,7 +41,7 @@ export class NewContactComponent implements OnInit {
       phone: value.phone,
       email: value.email
     }
-    console.log(this.userData)
+    console.log(this.newUserForm)
 
     // this.crudService.createNewQuestion(this.questionData)
     //   .then(
